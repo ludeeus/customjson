a9f5c46c7e83c82f450c7bf3617ce0242b7eea9e
ludeeus <joasoe@gmail.com>
2019-01-07 01:24:50 +0100

    fix component push

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
     LONG = fh.read()
 setuptools.setup(
     name="customjson",
-    version="1.1.1",
+    version="1.1.2",
     author="Joakim Sorensen",
     author_email="ludeeus@gmail.com",
     description="",
